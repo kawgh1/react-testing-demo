35ab2326fe305aea442d21b9eba561c31d9af9d2
kawgh1 <krseven@gmail.com>
2021-12-16 10:20:30 -0600

    notes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <h1>learn react testing</h1>
+            <h1>learn react testing!!</h1>
         </div>
     );
 }
